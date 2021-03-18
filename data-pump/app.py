f8ccd44f580f4f65812b462d0ef29e4a42d70190
Garrett Plasky <gplasky@evernote.com>
2021-03-17 19:05:27 -0700

    Initial app and Dockerfile scaffolding

---
+++ data-pump/app.py
@@ -0,0 +1,18 @@
+# HW5 requirements
+#  At least one function
+#  At least one if statement (can be if/else or if/elif/else)
+#  At least one list OR one dictionary
+#  Some level of communication or interaction with a user (printed status updates on code as it runs, requests for inputs, or reading in data from the user’s (your) computer)
+#  At least one library
+
+class GarimDataPump():
+
+    def __init__(self):
+        return
+
+    def sync_data(self):
+        return
+
+
+if __name__ == "__main__":
+    GarimDataPump.sync_data()
